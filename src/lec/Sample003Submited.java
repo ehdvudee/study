659b50a6885db4281937a5d3c5b43d3485e03496
ehdvudee <ehdvudee@naver.com>
2019-05-26 15:32:10 +0900

    add submitted test from A

---
+++ src/lec/Sample003Submited.java
@@ -0,0 +1,17 @@
+package lec;
+
+public class Sample003Submited {
+    public static void main( String[] args ) {
+
+        for (int i = 0; i <= 10; i++) {
+
+            for (int j = 0; j <= i; j++)
+
+                System.out.println("*");
+
+            System.out.println();
+
+        }
+
+    }
+}
